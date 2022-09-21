b7eda90ccc936a61f5e2bb263ee27ca33e61d9da
HY. Kelvin Lee <hykelvinlee42@gmail.com>
2022-09-21 11:25:35 -0400

    resolved auto login bug

--- bing.py
+++ bing.py
@@ -1,15 +1,14 @@
+import time
+import argparse
+import platform
+from datetime import date
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import platform
-import time
-from datetime import date
-from helpers.decrypt import get_passcode
+import helpers.elements as Elements
 import helpers.browsers as BrowserHelpers
+from helpers.decrypt import get_passcode
 
 search_array = []
-LOGIN_BAR_ELEMENT_NAME = "loginfmt"
-LOGIN_PASSWORD_INPUT_ELEMENT_NAME = "passwd"
-SEARCH_BAR_ELEMENT_ID = "sb_form_q"
 
 
 def load_search_array(filename="./search_keys"):
@@ -20,28 +19,32 @@ def load_search_array(filename="./search_keys"):
 
 
 def login(driver):
-    driver.get("https://login.live.com/")
+    driver.get("https://www.bing.com/news/")
+    time.sleep(2)
 
-    email_input = driver.find_element(By.NAME, LOGIN_BAR_ELEMENT_NAME)
+    login_form = driver.find_element(By.NAME, Elements.LOGIN_BTN_ELEMENT_NAME)
+    login_form.click()
+    time.sleep(2)
+
+    email_input = driver.find_element(By.NAME, Elements.LOGIN_BAR_ELEMENT_NAME)
     email_input.send_keys(input("Account: "))
     email_input.send_keys(Keys.ENTER)
 
     time.sleep(2)
-    password_input = driver.find_element(By.NAME, LOGIN_PASSWORD_INPUT_ELEMENT_NAME)
+    password_input = driver.find_element(By.NAME, Elements.LOGIN_PASSWORD_INPUT_ELEMENT_NAME)
     password_input.send_keys(get_passcode())
 
     for _i in range(2): # repeat one more time for confirming stay login
         time.sleep(1)
-        submit_btn = driver.find_element(By.ID, "idSIButton9")
+        submit_btn = driver.find_element(By.ID, Elements.LOGIN_SUBMIT_BTN_ELEMENT_ID)
         submit_btn.submit()
-
-    driver.get("https://account.microsoft.com/rewards/") # redirect to microsoft rewards
+    
     return True
 
 
 def search(driver, key):
     print("Seaching " + key + "...")
-    search_bar = driver.find_element(By.ID, SEARCH_BAR_ELEMENT_ID)
+    search_bar = driver.find_element(By.ID, Elements.SEARCH_BAR_ELEMENT_ID)
     search_bar.send_keys(key)
     time.sleep(2)
     search_bar.send_keys(Keys.ENTER)
@@ -51,7 +54,7 @@ def search(driver, key):
 def open_tabs_for_search(driver):
     for index, key in enumerate(search_array):
         tab_name = "tab" + str(index)
-        window_cmd = "window.open('about:blank', " + "'" + tab_name + "')"
+        window_cmd = "window.open('about:blank', '{0}')".format(tab_name)
 
         driver.execute_script(window_cmd)
         driver.switch_to.window(tab_name)
@@ -60,20 +63,27 @@ def open_tabs_for_search(driver):
         search(driver, key)
 
 
+def usages():
+    parser = argparse.ArgumentParser(description="Automation script to complete Microsoft Rewards - Bing search daily challenge")
+    args = parser.parse_args()
+    return vars(args)
+
+
 def main():
+    args = usages()
+
     load_search_array()
     is_login = False
     start_time = time.time()
+
     if platform.system() == "Linux": 
         browser = BrowserHelpers.get_linux_chrome()
-        browser.get("https://www.bing.com/")
         is_login = login(browser)
     elif platform.system() == "Windows":
         browser = BrowserHelpers.get_wins_edge()
-        browser.get("https://account.microsoft.com/rewards/")
         is_login = True
-    else:
-        return
+    
+    browser.get("https://account.microsoft.com/rewards/")
 
     if is_login:
         open_tabs_for_search(browser)
